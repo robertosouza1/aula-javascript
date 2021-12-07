82fd78fca3fd3e77d05a51908e743cb622550be4
robertosouza1 <robertosouza1@gmail.com>
2021-12-06 22:26:25 -0300

    Incluindo documentação

--- teste.js
+++ teste.js
@@ -5,3 +5,14 @@
 /* Comentario de varias linhas
 var z = 2;
 */
+
+// Declarando função
+function soma(a, b) {
+    return a + b;
+}
+
+// Chamada da função
+soma(3, 5);
+
+// Exibe saida no console
+console.log("Hello World!");
